7581bdfec552e6f67689cdcce83efe3cab178897
Fariha-Nasmee <farihanasmee@gmail.com>
2025-10-29 20:32:16 +0600

    Update MenuUI.cs

--- Chess-Challenge/src/Framework/Application/UI/MenuUI.cs
+++ Chess-Challenge/src/Framework/Application/UI/MenuUI.cs
@@ -1,4 +1,4 @@
-﻿using Raylib_cs;
+using Raylib_cs;
 using System.Numerics;
 using System;
 using System.IO;
@@ -14,25 +14,18 @@ namespace ChessChallenge.Application
             float spacing = buttonSize.Y * 1.2f;
             float breakSpacing = spacing * 0.6f;
 
-            // Game Buttons
-            if (NextButtonInRow("Human vs MyBot", ref buttonPos, spacing, buttonSize))
+            
+            if (NextButtonInRow("Start Game", ref buttonPos, spacing, buttonSize))
             {
-                var whiteType = controller.HumanWasWhiteLastGame ? ChallengeController.PlayerType.MyBot : ChallengeController.PlayerType.Human;
-                var blackType = !controller.HumanWasWhiteLastGame ? ChallengeController.PlayerType.MyBot : ChallengeController.PlayerType.Human;
-                controller.StartNewGame(whiteType, blackType);
-            }
-            if (NextButtonInRow("MyBot vs MyBot", ref buttonPos, spacing, buttonSize))
-            {
-                controller.StartNewBotMatch(ChallengeController.PlayerType.MyBot, ChallengeController.PlayerType.MyBot);
-            }
-            if (NextButtonInRow("MyBot vs EvilBot", ref buttonPos, spacing, buttonSize))
-            {
-                controller.StartNewBotMatch(ChallengeController.PlayerType.MyBot, ChallengeController.PlayerType.EvilBot);
+               controller.StartNewBotMatch(
+                    ChallengeController.PlayerType.MyBot,
+                    ChallengeController.PlayerType.EvilBot
+                );
+                ConsoleHelper.Log($"Starting match: {Settings.Bot1Name} vs {Settings.Bot2Name}");
             }
 
-            // Page buttons
+            
             buttonPos.Y += breakSpacing;
-
             if (NextButtonInRow("Save Games", ref buttonPos, spacing, buttonSize))
             {
                 string pgns = controller.AllPGNs;
@@ -43,28 +36,15 @@ namespace ChessChallenge.Application
                 File.WriteAllText(fullPath, pgns);
                 ConsoleHelper.Log("Saved games to " + fullPath, false, ConsoleColor.Blue);
             }
-            if (NextButtonInRow("Rules & Help", ref buttonPos, spacing, buttonSize))
-            {
-                FileHelper.OpenUrl("https://github.com/SebLague/Chess-Challenge");
-            }
-            if (NextButtonInRow("Documentation", ref buttonPos, spacing, buttonSize))
-            {
-                FileHelper.OpenUrl("https://seblague.github.io/chess-coding-challenge/documentation/");
-            }
-            if (NextButtonInRow("Submission Page", ref buttonPos, spacing, buttonSize))
-            {
-                FileHelper.OpenUrl("https://forms.gle/6jjj8jxNQ5Ln53ie6");
-            }
 
-            // Window and quit buttons
             buttonPos.Y += breakSpacing;
-
             bool isBigWindow = Raylib.GetScreenWidth() > Settings.ScreenSizeSmall.X;
             string windowButtonName = isBigWindow ? "Smaller Window" : "Bigger Window";
             if (NextButtonInRow(windowButtonName, ref buttonPos, spacing, buttonSize))
             {
                 Program.SetWindowSize(isBigWindow ? Settings.ScreenSizeSmall : Settings.ScreenSizeBig);
             }
+
             if (NextButtonInRow("Exit (ESC)", ref buttonPos, spacing, buttonSize))
             {
                 Environment.Exit(0);
